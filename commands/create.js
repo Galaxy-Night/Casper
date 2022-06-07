a0a9a1e83ea6471be56b40263b43e745bf509f82
Galaxy-Night <meliaerinelizabeth@gmail.com>
2022-06-07 09:51:44 -0700

    create and delete commands done

---
+++ commands/create.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: 'create',
+	async execute(interaction, db) {
+		const collection = db.collection(String(interaction.guildId));
+		const name = interaction.options.getString('name');
+
+		// make sure no other characters exist with the given name
+		if (await collection.findOne({ nameLower: name.toLowerCase() })) {
+			await interaction.reply(`${name} already exists!`);
+			return;
+		}
+
+		const character = require('./characterBase.json');
+		character.name = name;
+		character.nameLower = name.toLowerCase();
+
+		const result = await collection.insertOne(character);
+		if (!result.insertedId)
+			await interaction.reply(`Something went wrong and I couldn't create ${name}!`);
+		else
+			await interaction.reply(`${name} has been created!`);
+	}
+};
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Intents, Collection } = require('discord.js');
 const { token, dbUri } = require('./config.json');
 const { MongoClient } = require('mongodb');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
 const dbClient = new MongoClient(dbUri);
 let db;
+
+// dynamically generate command list
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 // fires when the client has connected to Discord
 client.once('ready', async () => {
@@ -19,6 +28,28 @@ client.once('ready', async () => {
 		console.error(err);
 	}
 	console.log('Connected to Discord');
+});
+
+// Executes commands
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction, db);
+	}
+	catch (e) {
+		console.error(e);
+		await interaction.reply({ content: 'Something went wrong and I couldn\'t execute that command!',
+			ephemeral: true });
+	}
+});
+
+client.on('guildDelete', async guild => {
+	db.collection(String(guild.id)).drop();
 });
 
 client.login(token);
