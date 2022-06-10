2d43bb617cd5be165de584a201c4383b26e5c133
Galaxy-Night <meliaerinelizabeth@gmail.com>
2022-06-10 13:48:12 -0700

    User can now override modifier on ability score checks

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 
-module.exports = { generateSubcommands, runSubcommand };
+module.exports = { generateSubcommands, runSubcommand, runSubcommandOrGroup };
 
 /**
  * Generates a map of subcommand names and modules to be passed to runSubcommand
@@ -22,12 +22,28 @@ function generateSubcommands(path) {
 /**
  * Runs a subcommand from a command
  *
- * @param {*} subcommands the Map containing the subcommands as produced by generateSubcommands
- * @param {*} interaction the interaction that triggered the command
- * @param {*} db          the database object needed for the subcommand
+ * @param {Map}                subcommands the Map containing the subcommands as produced by generateSubcommands
+ * @param {CommandInteraction} interaction the interaction that triggered the command
+ * @param {db}                 db          the database object needed for the subcommand
  */
 async function runSubcommand(subcommands, interaction, db) {
 	const key = interaction.options.getSubcommand();
+	const subcommand = subcommands.get(key);
+	if (!subcommand) return;
+
+	try {
+		await subcommand.execute(interaction, db);
+	}
+	catch (e) {
+		console.error(e);
+		await interaction.reply({ content: 'Something went wrong and I couldn\'t execute that command!',
+			ephemeral: true });
+	}
+}
+
+async function runSubcommandOrGroup(subcommands, interaction, db) {
+	const key = interaction.options.getSubcommandGroup(false) === null ? interaction.options.getSubcommand() :
+		interaction.options.getSubcommandGroup();
 	const subcommand = subcommands.get(key);
 	if (!subcommand) return;
 
